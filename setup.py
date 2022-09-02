15acb10105428e974bbc64831d36fda290eefbed
saarthaksangam <saarthaksangamnerkar@gmail.com>
2022-09-02 15:45:49 -0400

    adding initial reports

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo will house the data science project for the Olympics Men's and Women's Short track Speed Skating team for Italy Olympics 2026.',
+    author='Saarthak Sangamnerkar',
+    license='MIT',
+)
